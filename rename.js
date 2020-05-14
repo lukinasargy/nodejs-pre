ce3209c8ad3ec433dde39a970aa005cc55c28c0b
Sargy Lukina <lukina_sargy@mail.ru>
2020-05-15 00:51:34 +0300

    first commit

---
+++ rename.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+if (!fs.existsSync('./assets/img')) {
+		fs.mkdirSync('./assets/img');
+	};
+fs.readdir('./assets', (err, files) => {
+	files.forEach((file, index) => {
+		if (file.endsWith('jpg') || file.endsWith('png') || file.endsWith('svg')) {
+			fs.rename('./assets/' + file, './assets/img/' + file, (err) => {
+				if (err) throw err;
+				console.log('rename completed');
+			});
+		} else if (file.endsWith('Без названия')) {
+			let fileArray = file.split('.');
+//			console.log(fileArray);
+			let newname = fileArray.slice(0, (fileArray.length - 1)).join('.');
+			fs.rename('./assets/' + file, './assets/' + newname, (err) => {
+				if (err) throw err;
+				console.log('rename completed');
+			});
+		}
+	});
+});
